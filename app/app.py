f4bdefde3379d5403702ffc6bf31e5ea2269b8a0
Belllesssss <annshinriu@gmail.com>
2026-03-03 16:10:23 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,5 +32,9 @@ def index():
     return render_template('index.html', tickets=load_tickets()[::-1]) # Newest first
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
